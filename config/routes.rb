0ddbdee97d69756b3bf37b08e043508a875fdbc8
davidrzuluaga <git@davidrzuluaga.com>
2018-08-15 20:31:22 -0500

    implementacion bootstrap 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
   get '/artists', to: 'artists#index', as: 'artists'
-  get 'artists/:id', to: 'artists#album'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'artists/:id', to: 'artists#album', as: 'artist'
 end
